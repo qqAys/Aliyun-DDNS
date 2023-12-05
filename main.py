978ac08ad4ac88db513c45543c3b4e4442cb1273
Jinx <me@qqays.xyz>
2023-12-05 16:22:37 +0800

    移除lorem ipsum

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
+#  Copyright (c) 2021 - 2023 qqAys.
 
-#  Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-#  Vestibulum commodo. Ut rhoncus gravida arcu.
-
-# @Time          : 2023/12/05 10:26
+# @Time          : 2023/01/14 10:26
 # @Author        : Jinx
 # @Email-Private : me@qqays.xyz
 # @Github        : https://github.com/qqAys
